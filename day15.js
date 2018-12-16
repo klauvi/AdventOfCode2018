dd4077b9f2a38cd4c324811e8fade74657661377
Finnur Eiríksson <finnur@sensa.is>
2018-12-16 23:44:44 +0000

    Shortest path function ready and working

--- day15.js
+++ day15.js
@@ -19,7 +19,6 @@ const init = () => {
   const goblins = [];
   maxY = input.length;
   maxX = input[0].length;
-  console.log(maxY);
 
   for (let y = 0; y < maxY; y++) {
     map[y] = input[y].split('');
@@ -57,24 +56,107 @@ const inRange = (map, target, point) => {
   return points;
 };
 
+/**
+ *
+ * @param {Number[]} param0
+ * @param {Number[]} param1
+ */
+const distance = ([x1, y1], [x2, y2]) => Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+/**
+ *
+ * @param {any} map
+ * @param {Number[]} param1
+ */
+const getNeighbors = (map, [x, y]) => {
+  const neighbors = [];
+  if (y > 0) {
+    if (map[y - 1][x] === '.') {
+      neighbors.push([x, y - 1]);
+    }
+  }
+  if (map[y][x - 1] === '.') {
+    neighbors.push([x - 1, y]);
+  }
+  if (map[y][x + 1] === '.') {
+    neighbors.push([x + 1, y]);
+  }
+  if (y < maxY - 1) {
+    if (map[y + 1][x] === '.') {
+      neighbors.push([x, y + 1]);
+    }
+  }
+  return neighbors;
+};
+
+const reachable = (map, start, goal) => {
+  const startString = start.join(',');
+  const goalString = goal.join(',');
+  const closedSet = new Set();
+  const openSet = new Set();
+  openSet.add(startString);
+  const cameFrom = {};
+  const gScore = {};
+  gScore[startString] = 0;
+  const fScore = {};
+  fScore[startString] = distance(start, goal);
+  let current = '';
+  while (openSet.size > 0) {
+    let shortest = Infinity;
+    current = openSet.values().next().value;
+    openSet.forEach(point => {
+      if (fScore[point] < shortest) {
+        shortest = fScore[point];
+        current = point;
+      }
+    });
+    // console.log(openSet, fScore, current);
+    if (current === goalString) {
+      const path = [current];
+      while (cameFrom[current]) {
+        current = cameFrom[current];
+        path.push(current);
+      }
+      return path;
+    }
+    openSet.delete(current);
+    closedSet.add(current);
+    const neighbors = getNeighbors(map, current.split(',').map(Number));
+    for (let point of neighbors) {
+      const nbString = point.join(',');
+      if (!closedSet.has(nbString)) {
+        const tentative_gScore = gScore[current] + 1;
+        if (!openSet.has(nbString)) {
+          openSet.add(nbString);
+          fScore[nbString] = Infinity;
+        }
+        if (tentative_gScore >= gScore[nbString]) {
+          continue;
+        }
+        cameFrom[nbString] = current;
+        gScore[nbString] = tentative_gScore;
+        fScore[nbString] = gScore[nbString] + distance(point, goal);
+      }
+    }
+  }
+  return [];
+};
+
 const part1 = () => {
   const [map, elves, goblins] = init();
   const range = inRange(map, 'G', null);
-  console.log('Answer1:', range);
+  const path = reachable(map, [8, 16], [23, 13]);
+  console.log('Answer1:', path.length);
 };
-
-part1();
-
-const int = new Date().getTime();
-
 const part2 = () => {
   // insert part2 here, remember to refactor part1 to help with part2 solution 😊
 
   console.log('Answer2:');
 };
 
+part1();
+const int = new Date().getTime();
 part2();
-
 const end = new Date().getTime();
 
 console.log(`Finished in ${end - start}ms`);
