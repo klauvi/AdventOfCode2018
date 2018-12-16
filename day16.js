2ed365f809adaf36aef158043a60ebf1715755eb
Finnur Eiríksson <finnur@sensa.is>
2018-12-16 13:43:36 +0000

    Move input parsing to init function

--- day16.js
+++ day16.js
@@ -10,7 +10,26 @@ const init = () => {
   };
 
   const input = getData();
-  return [input];
+
+  const samples = [];
+  const tests = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].indexOf('Before') === 0) {
+      const before = input[i++].match(/\d/g).map(Number);
+      const op = input[i++].split(' ').map(Number);
+      const after = input[i++]
+        .match(/\d/g)
+        .map(Number)
+        .join('');
+      samples.push({ before, op, after });
+    } else {
+      const op = input[i].split(' ').map(Number);
+      if (op.length > 0) {
+        tests.push(op);
+      }
+    }
+  }
+  return [samples, tests];
 };
 
 const ops = {
@@ -79,36 +98,35 @@ const ops = {
     return [...register];
   }
 };
+const opcodes = {};
 
 const part1 = () => {
-  const [input] = init();
+  const [samples, tests] = init();
   const answer1 = [];
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].indexOf('Before') === 0) {
-      const before = input[i++].match(/\d/g).map(Number);
-      const op = input[i++].split(' ').map(Number);
-      const after = input[i++]
-        .match(/\d/g)
-        .map(Number)
-        .join('');
-      let matches = Object.keys(ops).reduce((acc, key) => {
-        const result = ops[key]([...before], op);
-        acc += result.join('') === after ? 1 : 0;
-        return acc;
-      }, 0);
-      if (matches >= 3) {
-        answer1.push(matches);
+  samples.forEach(sample => {
+    const { before, op, after } = sample;
+    const matches = Object.keys(ops).reduce((acc, key) => {
+      const result = ops[key]([...before], op);
+      if (result.join('') === after) {
+        acc.push(key);
       }
-    } else {
-      break;
+      return acc;
+    }, []);
+    // if (matches.length === 2) {
+    //   console.log(matches, before, op, after);
+    //   opcodes[op[0]] = matches[0];
+    // }
+    if (matches.length >= 3) {
+      answer1.push(matches.length);
     }
-  }
+  });
   console.log('Answer1:', answer1.length);
 };
 
 part1();
 const int = new Date().getTime();
 
+console.log(opcodes);
 const part2 = () => {
   // insert part2 here, remember to refactor part1 to help with part2 solution 😊
 
@@ -123,4 +141,21 @@ console.log(`First part in ${int - start}ms`);
 console.log(`Second part in ${end - int}ms`);
 
 /*
+{ '0': 0,
+  '1': 55,
+  '2': 145,
+  '3': 50,
+  '4': 102,
+  '5': 0,
+  '6': 94,
+  '7': 96,
+  '8': 48,
+  '9': 146,
+  '10': 0,
+  '11': 0,
+  '12': 0,
+  '13': 44,
+  '14': 0,
+  '15': 0 }
+
  */
