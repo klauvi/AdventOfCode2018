331f9666f120d3d078e68762054d5abb81542a6b
Finnur Eiríksson <finnur@sensa.is>
2018-12-19 01:09:26 +0000

    Day 15 single turn works on all tested schenarios

--- day15.js
+++ day15.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /*
 A* shortest path function -> https://en.wikipedia.org/wiki/A*_search_algorithm
 */
@@ -10,7 +11,8 @@ let maxX;
 
 const init = () => {
   const getData = () => {
-    const input = fs.readFileSync('./day15.txt');
+    const input = fs.readFileSync('./test.txt');
+    // const input = fs.readFileSync('./day15.txt');
     return input.toString().split('\n');
   };
 
@@ -25,38 +27,118 @@ const init = () => {
 
   for (let y = 0; y < maxY; y++) {
     map[y] = input[y].split('');
-    const hp = 200;
     let match;
     while ((match = regExp.exec(input[y])) !== null) {
       const x = match.index;
-      const object = { x, y, hp };
+      const object = new Entity(match[0], x, y);
       match[0] === 'G' ? goblins.push(object) : elves.push(object);
+      map[y][x] = object;
     }
   }
   return [map, elves, goblins];
 };
 
-const inRange = (map, target, point) => {
-  const points = [];
+const Entity = function(type, x, y) {
+  this.type = type;
+  this.enemy = type === 'G' ? 'E' : 'G';
+  this.x = x;
+  this.y = y;
+  this.hp = 200;
+};
+Entity.prototype.toString = function() {
+  return this.type;
+};
+Entity.prototype.turn = function(map) {
+  // check if enemy is in next hop and attack if there is
+  const enemy = getNextHop(map, this.x, this.y).find(
+    value => value && value.toString() === this.enemy
+  );
+  if (enemy) {
+    enemy.hp -= 3;
+    return true;
+  } else {
+    // No enemy, check if we can move
+    let going = [0, 0];
+    let shortest = Infinity;
+    getNeighbors(map, [this.x, this.y]).forEach(from => {
+      const [closest, distance] = getClosest(map, this.enemy, from);
+      if (distance < shortest) {
+        going = closest;
+        shortest = distance;
+      }
+    });
+    if (shortest < Infinity) {
+      // There is an open path to an enemy, let's move
+      map[going[1]][going[0]] = this;
+      map[this.y][this.x] = '.';
+      [this.x, this.y] = going;
+      // now we need to check again if there is enemy close and attack if there is
+      if (shortest === 1) {
+        // There should be enemy next
+        const enemy = getNextHop(map, this.x, this.y).find(
+          value => value && value.toString() === this.enemy
+        );
+        if (enemy) {
+          enemy.hp -= 3;
+          return true;
+        } else {
+          console.log('Someone is lying', this);
+        }
+      } else {
+        // there shouldnt be enemy close enough, but still check
+        const enemy = getNextHop(map, this.x, this.y).find(
+          value => value && value.toString() === this.enemy
+        );
+        if (enemy) {
+          enemy.hp -= 3;
+          console.log('Someone is lying again', this);
+        }
+        return true;
+      }
+    } else {
+      // nowhere to go, do nothing
+      return false;
+    }
+  }
+};
+
+const getClosest = (map, target, from) => {
+  let closest = from;
+  let distance = Infinity;
   for (let y = 0; y < maxY; y++) {
     for (let x = 0; x < maxX; x++) {
-      if (map[y][x] === target) {
-        if (map[y - 1] && map[y - 1][x] === '.') {
-          points.push({ x, y: y - 1 });
-        }
-        if (map[y][x - 1] === '.') {
-          points.push({ x: x - 1, y });
-        }
-        if (map[y][x + 1] === '.') {
-          points.push({ x: x + 1, y });
-        }
-        if (map[y + 1] && map[y + 1][x] === '.') {
-          points.push({ x, y: y + 1 });
-        }
+      if (map[y][x].toString() === target) {
+        getNeighbors(map, [x, y]).forEach((goal, index, array) => {
+          if (from.join('') === goal.join('')) {
+            // can't get closer than this, we can attack now
+            if (distance > 1) {
+              closest = from;
+              distance = 1;
+            }
+          }
+          const path = reachable(map, from, goal);
+          if (path && path.length < distance) {
+            closest = from;
+            distance = path.length;
+          }
+        });
       }
     }
   }
-  return points;
+  return [closest, distance];
+};
+
+const getNextHop = (map, x, y) => {
+  const next = [];
+  if (map[y - 1]) {
+    next.push(map[y - 1][x]);
+  }
+  next.push(map[y][x - 1]);
+  next.push(map[y][x + 1]);
+  if (map[y + 1]) {
+    next.push(map[y + 1][x]);
+  }
+  return next;
 };
 
 /**
@@ -113,7 +195,6 @@ const reachable = (map, start, goal) => {
         current = point;
       }
     });
-    // console.log(openSet, fScore, current);
     if (current === goalString) {
       const path = [current];
       while (cameFrom[current]) {
@@ -142,14 +223,20 @@ const reachable = (map, start, goal) => {
       }
     }
   }
-  return [];
+  return false;
 };
 
 const part1 = () => {
   const [map, elves, goblins] = init();
-  const range = inRange(map, 'G', null);
-  const path = reachable(map, [8, 16], [23, 13]);
-  console.log('Answer1:', path.length);
+  // const path = reachable(map, [8, 16], [23, 13]);
+  const elf = map[2][4];
+  const goblin = map[1][3];
+  console.log(goblin.turn(map));
+  console.log(goblin);
+  // for (let line of map) {
+  //   console.log(line.join(''));
+  // }
+  console.log('Answer1:', elf);
 };
 const part2 = () => {
   // insert part2 here, remember to refactor part1 to help with part2 solution 😊
