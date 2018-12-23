fe134121a0c70ec1b0ea3e75807b041b2cf5773a
Finnur Eiríksson <finnur@sensa.is>
2018-12-23 21:57:32 +0000

    Finished day 23 part 1

--- day23.js
+++ day23.js
@@ -19,7 +19,23 @@ const distance = (x1, y1, z1, x2, y2, z2) => {
 
 const part1 = () => {
   // insert part1 here
-  console.log('Answer1:');
+  const [input] = init();
+  let maxR = 0;
+  let maxIndex = -1;
+  for (let i = 0; i < input.length; i++) {
+    const [x, y, z, r] = input[i].match(/[-]*\d+/g).map(Number);
+    if (r > maxR) {
+      maxR = r;
+      maxIndex = i;
+    }
+  }
+  let inRange = 0;
+  const [strongX, strongY, strongZ, strongR] = input[maxIndex].match(/[-]*\d+/g).map(Number);
+  for (let line of input) {
+    const [x, y, z, r] = line.match(/[-]*\d+/g).map(Number);
+    inRange += distance(strongX, strongY, strongZ, x, y, z) <= strongR ? 1 : 0;
+  }
+  console.log('Answer1:', inRange);
 };
 
 const part2 = () => {
